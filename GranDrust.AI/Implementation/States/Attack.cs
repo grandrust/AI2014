0d6091ca2bf32e2c8d8cdc55d584a03d9833ec7f
Vladimir <grandrust@gmail.com>
2014-09-20 20:56:42 +0400

    Not commited files are added
    Related Work Items: #13

---
+++ GranDrust.AI/Implementation/States/Attack.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using GranDrust.AI.Core;
+using GranDrust.AI.Core.States;
+using GranDrust.AI.Implementation.Decisions;
+
+namespace GranDrust.AI.Implementation.States
+{
+    public class Attack : TeamState
+    {
+        public Attack()
+        {
+            StateType = TeamStateType.Attack;
+
+            Init();
+        }
+
+        private void Init()
+        {
+            Start = new Decision1();
+        }
+    }
+}
