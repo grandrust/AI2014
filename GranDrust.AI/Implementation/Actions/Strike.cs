512f5687b65b42354b3866202f8f54eb5d665ed7
Vladimir <grandrust@gmail.com>
2014-09-21 00:23:44 +0400

    added untracked file
    Related Work Items: #15

---
+++ GranDrust.AI/Implementation/Actions/Strike.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using GranDrust.AI.Core;
+using Com.CodeGame.CodeHockey2014.DevKit.CSharpCgdk.Model;
+
+using GameAction = GranDrust.AI.Core.Action;
+
+namespace GranDrust.AI.Implementation.Actions
+{
+    public class Strike: GameAction
+    {
+        protected Move Move { get { return Current.Move; } }
+        public override void Do()
+        {
+            var opponent = Current.World.GetOpponentPlayer();
+
+            //foreach (var hockeyist in Current.World.Hockeyists)
+            //{
+            //    if (!hockeyist.IsTeammate && hockeyist.Type == HockeyistType.Goalie)
+            //    {
+ 
+            //    }
+            //}
+
+            Move.SpeedUp = 1.0D;
+            Move.Turn = Current.Hockeyist.GetAngleTo(opponent.NetFront, opponent.NetBottom - Current.World.Puck.Radius);
+            Move.Action = ActionType.Strike;
+                     
+        } 
+    }
+}
